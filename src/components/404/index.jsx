6d660b6341f7abba87ec88061272fdf7353f245d
shengkai10517 <shengkai@datanchor.io>
2020-10-15 17:06:17 -0400

    added 404 page

---
+++ src/components/404/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Missing404 extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>404</div>
+      </div>
+    );
+  }
+}
+
+export default Missing404;
--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import HomeLayout from "../components/HomeLayout/index";
 import ProfilePage from "../components/MovieProfile/index";
 import SignInUp from "../components/SignInUp/index";
+import Page404 from "../components/404/index";
 
 const Web = (props) => (
   <Switch>
     <Route exact path="/" component={HomeLayout} />
     <Route exact path="/movie/:id" component={ProfilePage} />
     <Route exact path="/signin" component={SignInUp} />
+    <Route component={Page404} />
   </Switch>
 );
 
