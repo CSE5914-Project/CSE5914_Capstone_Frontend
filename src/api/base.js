4f7b8b3daa0a7fc218c9010a7c904b0682adb05c
Dragogo <dong.760@osu.edu>
2020-11-30 01:55:01 -0500

    update

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const IP_ADDRESS = "http://127.0.0.1:8000/";
+
+export const IP_ADDRESS = "https://cse5914-2020fall.herokuapp.com/";
+// export const IP_ADDRESS = "http://127.0.0.1:8000/";
 export const GET_QUESTION = "api/get_all_question/";
 export const GET_INTIAL_MOVIE = "api/get_popular_movies/";
 export const POST_ANSWER = "api/post_answer/";
